b17307e06d67fe700e6fbfcb5af7ec4468874918
maanis <manishjha962@gmail.com>
2025-05-11 14:34:13 +0530

    first commit

---
+++ basics/app.js
@@ -0,0 +1,7 @@
+var add = function (a, b) {
+    return a + b;
+};
+var a = 5;
+var b = 5;
+var result = add(a, b);
+console.log(result);
---
+++ basics/app.ts
@@ -0,0 +1,9 @@
+const add = (a: number, b: number) => {
+    return a + b;
+}
+
+const a = 5
+const b = 5
+
+const result = add(a, b);
+console.log(result);
