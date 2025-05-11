5e50bf106cf37efb1b20455acc2811d76f9c017e
maanis <manishjha962@gmail.com>
2025-05-11 14:44:01 +0530

    initial commit of app.js, app.ts, and index.html files

---
+++ objects/app.js
@@ -0,0 +1,6 @@
+var obj = {
+    name: 'John',
+    age: 30,
+    skills: ['nodejs', 'typescript', 56]
+};
+console.log(obj);
---
+++ objects/app.ts
@@ -0,0 +1,11 @@
+const obj: {
+    name: string,
+    age: number,
+    skills: [string, string, number, string?]
+} = {
+    name: 'John',
+    age: 30,
+    skills: ['nodejs', 'typescript', 56]
+}
+
+console.log(obj)
