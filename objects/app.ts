8cbd5ad211e306de92d99d798b11df29c789f4f0
maanis <manishjha962@gmail.com>
2025-05-14 11:29:37 +0530

    style: format object declaration for better readability

--- objects/app.ts
+++ objects/app.ts
@@ -1,11 +1,11 @@
 let obj: {
-    name: string,
-    age: number,
-    skills: [string, string, number, string?]
+  name: string,
+  age: number,
+  skills: [string, string, number, string?]
 } = {
-    name: 'John',
-    age: 30,
-    skills: ['nodejs', 'typescript', 56]
+  name: 'John',
+  age: 30,
+  skills: ['nodejs', 'typescript', 56]
 }
 
 console.log(obj)
@@ -13,3 +13,5 @@ console.log(obj)
 const skills: [string, number] = ['typescript', 56]
 
 console.log(skills)
+
+
